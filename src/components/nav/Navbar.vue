b52680c091a053bfabf3dd1b78f3c6216da280a9
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-05-04 08:20:36 +0200

    Fix

--- src/components/nav/Navbar.vue
+++ src/components/nav/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full mt-[33%] h-fit">
+  <div class="w-full mt-[17.5%] 2xl:mt-[25%] h-fit">
     <h1 class="font-bold text-xl sm:text-2xl my-4 mx-auto">
       Cisco Answers cap1-10
     </h1>
