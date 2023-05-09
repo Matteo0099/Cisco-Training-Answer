c86d08957204aab8eb0f00b47b22ca456660b7f5
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-05-09 11:46:36 +0200

    un casin

--- src/views/cap3.vue
+++ src/views/cap3.vue
@@ -2,15 +2,32 @@
   <div>
     <div class="mx-3 sm:mx-auto mt-8 min-h-screen">
       <h1 class="home text-2xl"> Answer of cap 3 </h1>
+      <!-- drag and drop -->
       <p>implementation drag and drop:</p>
-      <div class="container">
-        <p class="draggable" draggable="true">1</p>
-        <p class="draggable" draggable="true">2</p>
+      <div class="drop-zone">
+        <div
+          class="drag-el"
+          v-for="item in listTwo"
+          :key="item.title"
+          draggable
+          @dragstart="startDrag($event, item)"
+        >
+          {{ item.title }}
+        </div>
       </div>
-      <div class="container">
-        <p class="draggable" draggable="true">3</p>
-        <p class="draggable" draggable="true">4</p>
-      </div> <!-- container 1-->
+      <div class="drop-zone">
+        <div
+        class="drag-el"
+        v-for="item in listTwo"
+        :key="item.title"
+        draggable
+        @dragstart="startDrag($event, item)"
+      >
+        {{ item.title }}
+      </div>
+      </div>
+
+      <!-- container 1-->
       <ul class="min-h-screen list-none relative mt-4 pl-4">
         <li v-for="(question, index) in data.questions" class="py-4 m-2 sm:mx-auto px-2">
           <h1 class="home text-2xl"> {{ index + 1 }} - {{ question.question }} </h1>
@@ -34,68 +51,59 @@ import data from "../../src/data/ITE/3.json"
 export default {
   data() {
     return {
-      data
+      data,
+      items: [
+        {
+          id: 0,
+          title: 'Item A',
+          list: 1,
+        },
+        {
+          id: 1,
+          title: 'Item B',
+          list: 1,
+        },
+        {
+          id: 2,
+          title: 'Item C',
+          list: 2,
+        },
+      ],
     };
   },
-  mounted() {
-    const draggables = document.querySelectorAll('.draggable')
-
-    draggables.forEach(draggable => {
-      draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging')
-      })
-      draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging')
-      })
-    })
-
-    function getDragAfterElement(container, y) {
-      const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
-      return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height / 2;
-        if (offset < 0 && offset > closest.offset) {
-          return {
-            offset: offset,
-            element: child
-          };
-        } else {
-          return closest;
-        }
-      },
-        {
-          offset: Number.NEGATIVE_INFINITY,
-          element: null
-        }).element;
-    }
-
-    const containers = document.querySelectorAll('.container');
-    containers.forEach(container => {
-      container.addEventListener('dragover', e => {
-        e.preventDefault();
-        const draggable = document.querySelector('.dragging');
-        const afterElement = getDragAfterElement(container, e.clientY);
-        if (afterElement && container.contains(afterElement)) {
-          container.insertBefore(draggable, afterElement);
-        } else {
-          container.appendChild(draggable);
-        }
-      });
-    });
-  }
+  methods: {
+    startDrag(evt, item) {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('itemID', item.id)
+    },
+    onDrop(evt, list) {
+      const itemID = evt.dataTransfer.getData('itemID')
+      const item = this.items.find((item) => item.id == itemID)
+      item.list = list
+    },
+  },
+  computed: {
+    listOne() {
+      return this.items.filter((item) => item.list === 1)
+    },
+    listTwo() {
+      return this.items.filter((item) => item.list === 2)
+    },
+  },
 }
 </script>
 
-<style>
-.draggable {
-  padding: 1rem;
-  background-color: white;
-  border: 1px solid black;
-  cursor: grab;
+<style scoped>
+.drop-zone {
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
 }
 
-.draggable.dragging {
-  opacity: .5;
-  cursor: grabbing;
+.drag-el {
+  background-color: #fff;
+  margin-bottom: 10px;
+  padding: 5px;
 }
 </style>
