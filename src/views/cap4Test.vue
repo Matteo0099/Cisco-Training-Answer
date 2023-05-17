fee2433d07cc0485f2466b37bef9addf3d63a3c4
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-05-17 19:44:05 +0200

    Complete the check (forms) for every file.

--- src/views/cap4Test.vue
+++ src/views/cap4Test.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
     <!-- template for all caps -->
-    <form @submit.prevent="submitForm" class="flex flex-col gap-2 mt-10">
+    <form ref="examForm" @submit.prevent="submitForm" class="flex flex-col gap-2 mt-10">
       <h1 class="text-xl sm:text-2xl font-bold mt-8 mb-4">Test cap {{ numCap }} - complete </h1>
       <ul v-for="(question, index) in questions" :key="index" class="flex flex-col list-disc my-4">
         <h1 class="home text-xl sm:text-2xl mb-2"> {{ index + 1 }} - {{ question.question }} </h1>
         <template v-if="typeof question.answer === 'object'">
           <label v-for="(option, oIndex) in question.options" :key="oIndex" class="pl-4 text-base sm:text-lg">
-            <input type="checkbox" :value="option" v-model="selectedAnswers[index]" /> {{ option }}
+            <input type="checkbox" :value="option" v-model="selectedAnswers[index]" :disabled="formSubmitted" /> {{ option
+            }}
           </label>
         </template>
         <template v-else>
           <label v-for="(option, oIndex) in question.options" :key="oIndex" class="pl-4 text-lg sm:text-xl">
-            <input type="radio" :name="`radio-${index}`" :value="option" v-model="selectedAnswers[index]" />
+            <input type="radio" :name="`radio-${index}`" :value="option" v-model="selectedAnswers[index]"
+              :disabled="formSubmitted" />
             {{ option }}
           </label>
         </template>
       </ul>
       <button type="submit" role="button" :disabled="formSubmitted"
-        class="py-2 px-4 border rounded-lg mt-8 mb-4 w-72 active:border-4 font-semibold active:border-neutral-200 hover:opacity-75 h-14 mx-auto">
+        class="py-2 px-4 border rounded-lg w-72 active:border-4 font-semibold active:border-neutral-200 hover:opacity-75 h-14 mx-auto">
         <span v-if="!formSubmitted">Submit</span>
         <span v-else>Submitting...</span>
       </button>
+      <button type="button" role="button"
+        class="py-2 px-4 border rounded-lg w-72 active:border-4 font-semibold active:border-neutral-200 hover:opacity-75 h-14 mx-auto"
+        @click="refreshForm">
+        <span><i class="bi bi-arrow-clockwise"></i></span>
+      </button>
     </form>
     <p v-if="submitted" class="font-semibold text-xl sm:text-2xl mt-8 mb-12 mx-auto w-fit">
-      You got {{ correctAnswers }} out of {{ totalQuestions }} correct !
+      You got {{ correctAnswers }} out of {{ totalQuestions }} correct!
     </p>
   </div>
-</template> 
+</template>
 
 <script>
 import data from "../../src/data/ITE/4.json";
 export default {
   data() {
-    data.questions.sort(() => Math.random() - 0.5)
-    data.questions.forEach(question => question.options.sort(() => Math.random() - 0.5))
+    data.questions.sort(() => Math.random() - 0.5);
+    data.questions.forEach((question) => question.options.sort(() => Math.random() - 0.5));
     return {
       questions: data.questions,
       selectedAnswers: [],
@@ -42,7 +49,7 @@ export default {
       correctAnswers: 0,
       totalQuestions: 0,
       numCap: data.examData.cap,
-      formSubmitted: false
+      formSubmitted: false,
     };
   },
   created() {
@@ -54,14 +61,18 @@ export default {
       this.formSubmitted = true;
       await this.checkAnswers();
       this.submitted = true;
+      setTimeout(() => {
+        this.formSubmitted = false;
+      }, 2000);
     },
     checkAnswers() {
+      this.correctAnswers = 0;
       this.questions.forEach((question, index) => {
         const selected = this.selectedAnswers[index];
         const correctAnswer = question.answer;
         if (Array.isArray(correctAnswer)) {
           if (Array.isArray(selected)) {
-            selected.forEach(answer => {
+            selected.forEach((answer) => {
               if (correctAnswer.includes(answer)) {
                 this.correctAnswers++;
               }
@@ -77,7 +88,13 @@ export default {
           }
         }
       });
-    }
-  }
-}; 
+    },
+    refreshForm() {
+      this.formSubmitted = false;
+      this.submitted = false;
+      this.selectedAnswers = this.questions.map(() => []);
+      this.correctAnswers = 0;
+    },
+  },
+};
 </script>
