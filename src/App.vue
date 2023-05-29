7237e844e11f632968dd054ba858cd9dcaae36a0
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-05-29 21:56:49 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@ import btnDark from './components/btn-dark.vue';
 <template>
   <div class="w-full max-w-full h-[100vh] font-sans min-h-full relative top-0" data-aos="fade-down"
     data-aos-anchor-placement="bottom" data-aos-duration="375" data-aos-dealy="0">
-    <div class="btn-dark fixed right-4 top-2 md:top-8 md:right-16">
+    <div class="btn-dark w-fit mt-4 ml-auto mr-4 md:fixed md:top-8 md:right-16">
       <btnDark data-aos="zoom-in" data-aos-duration="375" data-aos-delay="125" type="button" role="button" />
       <!-- <translate /> --><!-- google translate (to implement => api google.translate.it)-->
-      <div id="google_translate_element"></div>
+      <div id="google_translate_element" class="w-max"></div>
     </div>
     <main class="container relative min-h-screen font-sans grid px-4 sm:p-0 sm:mx-auto max-w-2xl rounded-xl">
       <!-- <Navbar></Navbar> -->
@@ -37,30 +37,17 @@ import btnDark from './components/btn-dark.vue';
 /* icons */
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css");
 
-#google_translate_element {
-  position: absolute !important;
-}
-
 @media screen and (max-width: 999px) {
-
   .goog-te-gadget span,
   .goog-te-gadget span a {
     display: none !important;
   }
 }
 
-@media screen and (min-width: 999px) {
-  #google_translate_element {
-    top: 4.5rem !important;
-    right: 0.75rem !important;
-    margin-top: 1rem !important;
-    left: auto !important;
-  }
-}
-
 #google_translate_element {
   left: 0.75rem;
-  top: 1.25rem;
+  margin-top: 1rem;
+  min-width: 140px;
 }
 
 .goog-te-gadget {
