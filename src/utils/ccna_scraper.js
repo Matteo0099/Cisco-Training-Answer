c0e1e65c3d97506dddca3de80020bdd3067afab6
snakeout96 <86480748+snakeout96@users.noreply.github.com>
2023-10-28 23:09:52 +0200

    Create ccna_scraper.js

---
+++ src/utils/ccna_scraper.js
@@ -0,0 +1,49 @@
+/**
+*   console script to scraper answers (exam details below in obj schema) 
+*   into a JavaScript object from domain (https://itexamanswers.net/*)'s payload
+*
+*   !!! WARNING !!!
+*
+*   ONLY MULTIPLE CHOICE QUESTIONS (MOST OF THEM) ARE SCRAPERED. 
+*   !!! COMPLETION QUESTIONS AND DRAG-AND-DROP EXERCISES ARE OMITTED !!!
+*
+*   by noyazz
+*/
+
+const optionsLists = document
+  .querySelector('div.thecontent.clearfix')
+  .querySelectorAll('p ~ ul')
+
+const obj = {
+    "examData": {
+        "holder": "Cisco Netacad",
+        "examName": "CCNAv7: Introduction to Networks",
+        "abbreviation": "ITN",
+        "cap": 3,
+        "versionName": null,
+        "versions": [
+            5.1,
+            6.0
+        ],
+        "years": [
+            2020
+        ]
+    },
+    "questions": []
+}
+
+optionsLists.forEach(optionsList => {
+    const questionContainer = optionsList.previousElementSibling
+    const options = [ ...optionsList.children ]
+    
+    obj.questions.push(
+        {
+            "question": questionContainer.textContent.replace(/^\d+\.\s/, ''),
+            "options": options.map(option => option.textContent),
+            "answers": options.filter(option => /<span style="color: #ff0000;">/.test(option.innerHTML)).map(option => option.textContent),
+            "img": questionContainer.querySelector('img[src]')?.src
+        }
+    )
+})
+
+console.log(JSON.stringify(obj).replace(/\\n/g, ''))
